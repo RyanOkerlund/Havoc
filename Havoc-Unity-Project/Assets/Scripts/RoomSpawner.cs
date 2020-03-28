95c475939854498643d081a7b572df2d7f7a8dc4
RyanOkerlund <ryanokerlund13@gmail.com>
2020-03-27 22:18:08 -0600

    Basic Procedural Generation Implemented.
    Known Bug: Closed room still spawning on the entry room

--- Havoc-Unity-Project/Assets/Scripts/RoomSpawner.cs
+++ Havoc-Unity-Project/Assets/Scripts/RoomSpawner.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int openingDirection;
+    // 1 = need bottom door
+    // 2 = need top door
+    // 3 = need left door 
+    // 4 = need right door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool hasSpawned = false;
+
+    private void Start()
     {
-        
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        if (!hasSpawned)
+        {
+            if (openingDirection == 1)
+            {
+                // Need to spawn a room with a BOTTOM door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 2)
+            {
+                // Need to spawn a room with a TOP door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 3)
+            {
+                // Need to spawn a room with a LEFT door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 4)
+            {
+                // Need to spawn a room with a RIGHT door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+            }
+            hasSpawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SpawnPoint"))
+        {
+            if (collision.GetComponent<RoomSpawner>().hasSpawned == false && hasSpawned == false)
+            {
+                // Spawn closed room
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(this.gameObject);
+            }
+            hasSpawned = true;
+        }
     }
 }
---
+++ Havoc-Unity-Project/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
+    public GameObject closedRoom;
+}
